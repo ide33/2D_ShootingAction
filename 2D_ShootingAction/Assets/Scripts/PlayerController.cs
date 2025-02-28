e955369aa0bf43f9f7e381218c6bbe610240e3f3
kuzuboshi <Human.kagoshima.IdeRyouta@gmail.com>
2025-03-01 00:37:39 +0900

    プレイヤー 攻撃実装途中

--- 2D_ShootingAction/Assets/Scripts/PlayerController.cs
+++ 2D_ShootingAction/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,20 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;  // プレイヤーの速度
     [SerializeField] private float jumpForce = 5f;  // ジャンプの強さ
+    [SerializeField] GameObject ScaffoldAmmo;  // 足場のプレハブ
+    [SerializeField] GameObject MoveScaffoldAmmo;  // 移動足場のプレハブ
+    [SerializeField] GameObject CannonAmmo;  // 大砲のプレハブ
+    [SerializeField] Transform shootPoint;  // 弾の発射位置
 
-    private bool isGround = false;
+    private bool isGround = false;  // 地面にいるかどうか
     private Rigidbody2D rb2d;  // Rigidbodyの変数
+    // private Vector2 shootDirection = Vector2.right;  // デフォルトの発射方向
+    private SpriteRenderer spriteRenderer;  // スプライトの変数
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();  // Rigidbodyを取得
+        spriteRenderer = GetComponent<SpriteRenderer>(); // spriteRendererの初期化
     }
 
     void Update()
@@ -25,6 +32,38 @@ public class PlayerController : MonoBehaviour
         // 左右移動
         float horizontal = Input.GetAxis("Horizontal");  // 左右移動の入力をhorizontalに格納
         transform.Translate(Vector2.right * horizontal * speed * Time.deltaTime);  // horizontalで取得した方向に速度を掛ける
+
+        if(horizontal > 0)
+        {
+            // shootDirection = Vector2.right;  // 発射方向を右向き
+            spriteRenderer.flipX = false;  //スプライトを通常の向きで表示
+        }
+        else if (horizontal < 0)
+        {
+            // shootDirection = Vector2.left;  // 発射方向を左向き
+            spriteRenderer.flipX = true;  //スプライトを左右反転した向きで表示
+        }
+
+        // if(Input.GetKey(KeyCode.W)) shootDirection = Vector2.up;  // 発射方向を上向き
+        // if(Input.GetKey(KeyCode.S)) shootDirection = Vector2.down;  // 発射方向を上向き
+
+        // jキーが押されたら
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            Instantiate(ScaffoldAmmo, shootPoint.position, transform.rotation);   // 足場弾を生成
+        }
+
+        // kキーが押されたら
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            Instantiate(MoveScaffoldAmmo, shootPoint.position, transform.rotation);  // 移動足場弾を生成
+        }
+
+        // lキーが押されたら
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            Instantiate(CannonAmmo, shootPoint.position, transform.rotation);  // 大砲弾を生成
+        }
     }
 
     void OnCollisionStay2D(Collision2D collision)
